1b63d50ffe9f3a32a4ddaba9182f7386af67fb50
dwxdy <dwxdy130326@163.com>
2025-11-21 22:28:07 +0800

    只是尝试

--- main.ts
+++ main.ts
@@ -1,134 +1,134 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+// 记得重命名这些类和接口！
 
 interface MyPluginSettings {
-	mySetting: string;
+    mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+    mySetting: 'default'
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+    settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        // 在左侧功能区创建一个图标
+        const ribbonIconEl = this.addRibbonIcon('heart', '示例插件', (_evt: MouseEvent) => {
+            // 当用户点击图标时调用
+            new Notice('但是我想要的不是这个');
+        });
+        // 为功能区图标添加额外样式
+        ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        // 在应用底部添加状态栏项（在移动应用中不起作用）
+        const statusBarItemEl = this.addStatusBarItem();
+        statusBarItemEl.setText('状态栏文本');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        // 添加一个可以在任何地方触发的简单命令
+        this.addCommand({
+            id: 'open-sample-modal-simple',
+            name: '打开示例模态框（简单）',
+            callback: () => {
+                new SampleModal(this.app).open();
+            }
+        });
+        // 添加一个编辑器命令，可以在当前编辑器实例上执行某些操作
+        this.addCommand({
+            id: 'sample-editor-command',
+            name: '示例编辑器命令',
+            editorCallback: (editor: Editor, _view: MarkdownView) => {
+                console.log(editor.getSelection());
+                editor.replaceSelection('示例编辑器命令');
+            }
+        });
+        // 添加一个复杂命令，可以检查应用的当前状态是否允许执行该命令
+        this.addCommand({
+            id: 'open-sample-modal-complex',
+            name: '打开示例模态框（复杂）',
+            checkCallback: (checking: boolean) => {
+                // 要检查的条件
+                const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+                if (markdownView) {
+                    // 如果checking为true，我们只是"检查"命令是否可以运行
+                    // 如果checking为false，那么我们想要实际执行操作
+                    if (!checking) {
+                        new SampleModal(this.app).open();
+                    }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+                    // 只有当检查函数返回true时，该命令才会显示在命令面板中
+                    return true;
+                }
+            }
+        });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        // 添加设置选项卡，以便用户可以配置插件的各个方面
+        this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+        // 如果插件挂载了任何全局DOM事件（在不属于此插件的应用部分上）
+        // 使用此函数将在插件禁用时自动移除事件监听器
+        this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+            console.log('click', evt);
+        });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        // 注册间隔时，此函数将在插件禁用时自动清除间隔
+        this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+    }
 
-	onunload() {
+    onunload() {
+        // 插件卸载时的清理工作
+    }
 
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+    constructor(app: App) {
+        super(app);
+    }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    onOpen() {
+        const {contentEl} = this;
+        contentEl.setText('哇！');
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    onClose() {
+        const {contentEl} = this;
+        contentEl.empty();
+    }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: MyPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    display(): void {
+        const {containerEl} = this;
 
-		containerEl.empty();
+        containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+                .setName('设置 #1')
+                .setDesc('这是一个秘密')
+                .addText(text => text
+                        .setPlaceholder('输入你的秘密')
+                        .setValue(this.plugin.settings.mySetting)
+                        .onChange(async (value) => {
+                            this.plugin.settings.mySetting = value;
+                            await this.plugin.saveSettings();
+                        }));
+    }
 }
